b696cdc7c5eb7362f66b703a5813ebd314a9d226
jontx <jontx@10.245.75.208>
2026-09-25 10:17:58 +0200

    proba 3

--- src/main/java/edu/ncsu/monopoly/Card.java
+++ src/main/java/edu/ncsu/monopoly/Card.java
@@ -1,5 +1,5 @@
 package edu.ncsu.monopoly;
-//Eguneraketa irailak 25, bigarrena da hau
+//Eguneraketa irailak 25, bigarrena da hau, hirugarren proba
 public abstract class Card {
 
     public static final int TYPE_CHANCE = 1;
